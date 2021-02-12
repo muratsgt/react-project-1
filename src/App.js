6b32b26cbf2d015def5a19aa65f2d04bbc49baae
murat <muratakca.mt@gmail.com>
2021-02-12 18:02:25 +0100

    Navbar added, firebase added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
+
+  console.log(process.env);
+
   return (
     <div className="App">
+      <Navbar/>
       <Signup/>
 
 
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,32 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+// firestore
+
+const devConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID
+};
+
+const prodConfig = {}
+
+const config = process.env.NODE_ENV === "development" ? devConfig : prodConfig;
+
+class Firebase {
+    constructor(){
+        firebase.initializeApp(config);
+        this.firebaseAuth = firebase.auth();
+    }
+
+    // login signinWithEmailandPassword
+    // logout signOut
+    // forgot password
+    // register registerWithEmail
+    // sign in with google
+      
+}
+
+export default new Firebase();
