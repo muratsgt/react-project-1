7425b35469cca37eff346e6af299207988fbd225
murat <muratakca.mt@gmail.com>
2021-02-28 19:44:21 +0100

    Added userdetail

--- src/pages/UserDetail.js
+++ src/pages/UserDetail.js
@@ -1,20 +1,51 @@
-import React, {useEffect} from 'react';
-import {useParams} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { fetchData } from "../helper/FetchData";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Typography, Grid } from "@material-ui/core";
+import { format as formatDate, parseISO } from "date-fns";
+
+const stylesFunc = makeStyles((theme) => ({
+  wrapper: {
+    marginTop: "5rem",
+    height: "calc(100vh - 20rem)",
+    textAlign: "center",
+  },
+}));
 
 function UserDetail() {
+  const { id } = useParams();
+  const [userDetail, setUserDetail] = useState();
+  const mainStyles = stylesFunc();
 
-    const {id} = useParams();
+  useEffect(() => {
+    fetchData(`/user/${id}`)
+      .then((res) => setUserDetail(res))
+      .catch((err) => console.log(err))
+      .finally();
+  }, []);
 
-    useEffect(() => {
-        fetchData(`/user/${id}`)
-    }, [])
-    
-    return (
-        <div>
-            {id}
-        </div>
-    )
+  return (
+    <Container className={mainStyles.wrapper}>
+      <img
+        onClick={() => console.log("onClick")}
+        src={userDetail?.picture}
+        aspectRatio={4 / 3}
+      />
+      <Typography variant="h4">{userDetail?.firstName}</Typography>
+      <Typography variant="h4">{userDetail?.lastName}</Typography>
+      {userDetail?.dateOfBirth && (
+        <Typography variant="h4">
+          {
+            // TODO: move to helper
+          }
+          {formatDate(parseISO(userDetail.dateOfBirth), "MM/dd/yyyy")}
+        </Typography>
+      )}
+      <Typography variant="h4">{userDetail?.phone}</Typography>
+      {JSON.stringify(userDetail)}
+    </Container>
+  );
 }
 
-export default UserDetail
+export default UserDetail;
