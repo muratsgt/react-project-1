37b872f293a9963ec11d1f6369f15c61b83e5720
murat <muratakca.mt@gmail.com>
2021-03-13 20:40:23 +0100

    Pagination added

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,31 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container, Grid, LinearProgress } from "@material-ui/core";
+import { Container, Grid, CircularProgress } from "@material-ui/core";
 import MediaCard from "../components/MediaCard";
 import { fetchData } from "../helper/FetchData";
+import Pagination from "@material-ui/lab/Pagination";
+import Backdrop from "@material-ui/core/Backdrop";
+
+const RESULT_PER_PAGE = 12;
 
 const stylesFunc = makeStyles((theme) => ({
   wrapper: {
     marginTop: "5rem",
     textAlign: "center",
   },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
 }));
 
 function Main() {
   const [userData, setUserData] = useState(null);
+  const [page, setPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
 
   const mainStyles = stylesFunc();
 
   useEffect(() => {
-    fetchData(`/user`)
-      .then((res) => setUserData(res?.data))
+    fetchData(`/user?page=${page - 1}&limit=${RESULT_PER_PAGE}`)
+      .then((res) => {
+        setUserData(res?.data);
+        const num = Math.ceil(res?.total / RESULT_PER_PAGE);
+        setMaxPage(num);
+        setLoading(false);
+      })
       .catch((err) => console.log(err));
-  }, []);
+  }, [page]);
+
+  const changePage = (event, value) => {
+    setPage(value);
+    setLoading(true);
+  };
 
   return (
     <Container className={mainStyles.wrapper}>
+      <Backdrop className={mainStyles.backdrop} open={loading}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
       {!userData ? (
-          <LinearProgress />
+        <CircularProgress />
       ) : (
         <Grid
           container
@@ -48,6 +73,15 @@ function Main() {
               </Grid>
             );
           })}
+          <Grid container justify="center">
+            <Pagination
+              count={maxPage}
+              color="primary"
+              page={page}
+              onChange={changePage}
+              size="large"
+            />
+          </Grid>
         </Grid>
       )}
     </Container>
